8ca7cf9aebc2421ab7b8f893dd751af2373b911f
qingfeng <qingfeng@douban.com>
2013-03-17 15:44:16 +0800

    first commit

---
+++ status/admin.py
@@ -0,0 +1,18 @@
+#encoding: utf-8
+
+from datetime import datetime
+from django.contrib import admin
+from status.models import Status
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'ntime')
+    search_fields = ('text', )
+    list_filter = ('user', )
+    date_hierarchy = 'time'
+
+    def ntime(self, obj):
+        return obj.time.strftime('%Y-%m-%d %H:%M:%S')
+    ntime.short_description = u'时间'
+
+admin.site.register(Status, StatusAdmin)
