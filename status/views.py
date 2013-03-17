3ba9910e2e1d80f2581f809a5e3145307bcfb67e
qingfeng <qingfeng@douban.com>
2013-03-17 16:42:51 +0800

    add hello

--- status/views.py
+++ status/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+from status.models import Status
+
+
+def hello(request):
+    x = request.GET.get('x', 0)
+    x = int(x)
+    data = Status.objects.all()
+    return render(request, 'hello.html', {'result': x+1, 'data': data})
