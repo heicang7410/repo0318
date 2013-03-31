586764e114b6ea574c396129287e5c8be2d1bb7a
qingfeng <qingfeng@douban.com>
2013-03-31 16:27:47 +0800

    add follow model

--- status/models.py
+++ status/models.py
@@ -6,3 +6,8 @@ class Status(models.Model):
     text = models.CharField(max_length=200)
     time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User)
+
+class UserFollow(models.Model):
+    from_user = models.ForeignKey(User, related_name="following")
+    to_user = models.ForeignKey(User, related_name="follower")
+    time = models.DateTimeField(auto_now_add=True)
